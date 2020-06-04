a915600532d12ed8157cf99975994c58d9289625
Clémence <clemence.ptii@gmail.com>
2020-06-04 10:50:17 +0200

    test tag

--- bot.js
+++ bot.js
@@ -14,4 +14,16 @@ client.on('message', message => {
   if (message.content === "!bonjour"){
 	message.reply("Bonjour "+message.author.username);
   }
+  if (message.content.startWith('!salut')){
+	 if(message.mentions.user.size){
+	  const taggedUser = message.mentions.users.first();
+	  message.channel.send('Tu salues ${taggedUser.username}');
+	  message.channel.send('Salut <@'+taggedUser.author.id+'> !');
+	 }
+	 else{
+		 message.reply('Faut quelqu\'un à saluer !');
+	 }
+	  
+  }
+  
 });
