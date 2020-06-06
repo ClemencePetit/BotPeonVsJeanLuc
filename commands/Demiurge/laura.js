49dff5d468ea1667cd545d53be2e8ff4a5a81270
LadyCookie <bouzerea@etud.insa-toulouse.fr>
2020-06-06 13:20:23 -0400

    ajout classes board et keycode

---
+++ commands/Demiurge/laura.js
@@ -0,0 +1,24 @@
+const Board = require ("../../Classes/Board.js");
+
+module.exports = 
+{
+	name: 'laura',
+	description: 'Tests de Laura',
+	execute(DemiurgeBot, message, arguments)
+	{
+		var result = "";
+		var b = new Board();
+		
+		var myB = b.Board;
+		var name = b.name;
+		
+		
+		for(i=0;i<9;i++){
+			for(j=0;j<14;j++){
+				result += "[" + myB[i][j] + "]";
+			}
+			result += "\n";
+		}
+		message.channel.send(result);
+	},
+};
